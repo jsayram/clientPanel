c9c5ff2b0c33a77e9aed1dcb347c31ebe755f41a
Jose Ramirez <jsayram@Gmail.com>
2018-02-28 23:30:07 -0500

    Logout and navLinks implemented - user is succesfully logged out and navbar reflects when the user is logged in or logged out

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -4,7 +4,7 @@ import { ClientService } from '../../services/client.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { FlashMessagesService} from 'angular2-flash-messages';
 import { Client } from '../../models/Client';
-import {  } from 'angular2-flash-messages/module/module';
+
 
 
 
